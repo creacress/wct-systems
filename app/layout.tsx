3769e0e6fcba7ca20d85bb39198d59ff8b5a88bc
creacress <alexis@webcresson.com>
2026-03-01 09:53:07 +0100

    Implémentation google

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,7 +1,6 @@
-
-
 import "./globals.css";
 import type { Metadata } from "next";
+import { GoogleTagManager } from '@next/third-parties/google'
 
 export const metadata: Metadata = {
   metadataBase: new URL(process.env.NEXT_PUBLIC_SITE_URL ?? "http://localhost:3000"),
@@ -46,6 +45,7 @@ export default function RootLayout({
   return (
     <html lang="fr" suppressHydrationWarning>
       <body className="min-h-screen bg-background text-foreground antialiased">
+        <GoogleTagManager gtmId="GTM-5SG8GNR8" />
         {children}
       </body>
     </html>
